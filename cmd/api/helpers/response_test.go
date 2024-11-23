5f89137f0f26af8f5a9222500183fdb38c870a22
daut <bozidardautovic@gmail.com>
2024-11-23 18:54:07 +0100

    test: response helper

---
+++ cmd/api/helpers/response_test.go
@@ -0,0 +1,71 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/daut/jed/internal/assert"
+	"github.com/daut/jed/internal/utils"
+)
+
+func TestClientError(t *testing.T) {
+	t.Parallel()
+	responseHelper := NewResponse(utils.NewLogger())
+
+	tests := []struct {
+		Name                          string
+		Message                       string
+		ExpectedWWWAuthenticateHeader string
+		ExpectedStatus                int
+	}{
+		{Name: "Unauthorized", Message: "invalid token", ExpectedWWWAuthenticateHeader: "Bearer", ExpectedStatus: http.StatusUnauthorized},
+		{Name: "Bad Request", Message: "invalid input", ExpectedWWWAuthenticateHeader: "", ExpectedStatus: http.StatusBadRequest},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			responseHelper.ClientError(w, tt.Message, tt.ExpectedStatus)
+			resp := w.Result()
+			responseMessage := fmt.Sprintf(`{"message":"%s"}`, tt.Message)
+			assert.Equal(t, tt.ExpectedStatus, resp.StatusCode)
+			assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+			assert.Equal(t, tt.ExpectedWWWAuthenticateHeader, resp.Header.Get("WWW-Authenticate"))
+			assert.Equal(t, responseMessage, strings.TrimSpace(w.Body.String()))
+		})
+	}
+}
+
+func TestFailedValidation(t *testing.T) {
+	t.Parallel()
+	responseHelper := NewResponse(utils.NewLogger())
+
+	tests := []struct {
+		Name           string
+		Errors         map[string]string
+		ExpectedStatus int
+	}{
+		{Name: "Single error", Errors: map[string]string{"name": "invalid name"}, ExpectedStatus: http.StatusUnprocessableEntity},
+		{Name: "Multiple errors", Errors: map[string]string{"name": "invalid name", "price": "invalid price"}, ExpectedStatus: http.StatusUnprocessableEntity},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			responseHelper.FailedValidation(w, tt.Errors)
+			resp := w.Result()
+			assert.Equal(t, tt.ExpectedStatus, resp.StatusCode)
+			assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+			// unmarshal the response body to a map to compare the errors
+			var response map[string]any
+			err := json.NewDecoder(resp.Body).Decode(&response)
+			assert.Nil(t, err)
+			assert.Equal(t, "one or more validation errors occurred", response["message"])
+			assert.Equal(t, len(tt.Errors), len(response["errors"].(map[string]any)))
+		})
+	}
+}
--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -8,3 +8,10 @@ func Equal[T comparable](t testing.TB, expected, actual T) {
 		t.Errorf("expected: %v; got: %v", expected, actual)
 	}
 }
+
+func Nil(t testing.TB, actual any) {
+	t.Helper()
+	if actual != nil {
+		t.Errorf("expected: nil; got: %v", actual)
+	}
+}
